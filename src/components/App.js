df71b2c88df54b134f1a59ddc7ed1eb457445c59
Nitu Dubey <nitutiwari01@gmail.com>
2023-08-25 21:59:13 -0400

    fix prevbtn

--- src/components/App.js
+++ src/components/App.js
@@ -20,14 +20,15 @@ function handleRandomNum(){
         <div>
             <h1 id="review-heading">Our Reviews</h1>
             <div className="review">
-                <p id={"author-"+Reviews[index].id} className="author">{Reviews[index].name}</p>
+                <p id={"author -"+Reviews[index].id} className="author">{Reviews[index].name}</p>
                 <p className="job">{Reviews[index].job}</p>
                 <img className="person-img" src={Reviews[index].image} width="200" height="200" />
                 <p className="info">{Reviews[index].text}</p>
-            </div>
             <button className="prev-btn" onClick={()=>{if(index>0){setIndex(index-1)}}}>Previous</button>
             <button className="next-btn" onClick={()=>{if(index<Reviews.length-1){setIndex(index+1)}}}>Next</button>
             <button className="random-btn" onClick={handleRandomNum}>surprise me</button>
+            </div>
+
         </div>
     )
 }
